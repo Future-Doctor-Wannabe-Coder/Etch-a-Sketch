6327d293bf720f78d40ecfffc1f5ffd84f5ab1e4
Future-Doctor-Wannabe-Coder <zcm102297@gmail.com>
2023-07-19 15:32:11 -0400

    fixed buttons colors and interactions

--- script.js
+++ script.js
@@ -16,16 +16,33 @@ for (let i = 0; i < 256; i++) {
 
 let draw = document.createElement('button');
 draw.classList.add('draw');
-draw.textContent = "Draw";
+draw.textContent = "Black";
 buttonContainer.appendChild(draw);
+document.querySelector('.draw').style.backgroundColor = 'blue';
 
 //creating erase variable
 let erase = false;
 
+//creating clear variable
+let cleared = false;
+
 //Added so we can make sure hovering changes color only when click occurs on draw button
-let drawClick = false;
+let drawClick = true;
+
 document.querySelector('.draw').addEventListener('click', () => {
     drawClick = !drawClick;
+    if(drawClick === true) {
+        document.querySelector('.draw').style.backgroundColor = 'blue';
+        if (erase === true || cleared === true) {
+            erase = false;
+            cleared = false;
+            document.querySelector('.eraser').style.backgroundColor = 'white';
+            document.querySelector('.clear').style.backgroundColor = 'white';
+        }
+    }
+    else {
+        document.querySelector('.draw').style.backgroundColor = 'white';
+    }
     console.log('drawclick: ' + drawClick);
 });
 
@@ -33,12 +50,12 @@ for (let i = 0; i < 256; i++) {
     pixel[i].addEventListener('mouseover', () => {
         if(drawClick === true) {
             pixel[i].style.backgroundColor = 'black';
-            document.querySelector('.draw').style.backgroundColor = 'blue';
+            erase = false;
         }
         else {
-            document.querySelector('.draw').style.backgroundColor = 'white';
             pixel[i].style.backgroundColor = pixel[i].style.backgroundColor;
         }
+        console.log(drawClick);
     }); 
 }
 
@@ -50,11 +67,29 @@ clear.classList.add('clear');
 clear.textContent = "Clear";
 buttonContainer.appendChild(clear);
 
+
 for (let i = 0; i < 256; i++) {
     clear.addEventListener('click', () => {
         pixel[i].style.backgroundColor = 'white';
+        console.log("cleared: " + cleared);
     });
 }
+
+document.querySelector(".clear").addEventListener('click', () => {
+    cleared = !cleared;
+    if(cleared === true) {
+        document.querySelector('.clear').style.backgroundColor = 'blue';
+        if(drawClick === true || erase === true) {
+            erase = false;
+            drawClick = false;
+            document.querySelector('.draw').style.backgroundColor = 'white';
+            document.querySelector('.eraser').style.backgroundColor = 'white';
+        }
+    }
+    else {
+        document.querySelector('.clear').style.backgroundColor = 'white';
+    }
+});
 
 //Create eraser button
 let eraser = document.createElement('button');
@@ -64,7 +99,19 @@ buttonContainer.appendChild(eraser);
 
 document.querySelector('.eraser').addEventListener('click', () => {
     erase = !erase;
+    if(erase === true) {
+        document.querySelector('.eraser').style.backgroundColor = 'blue';
+        if(drawClick === true || cleared === true) {
+            drawClick = false;
+            cleared = false;
+            document.querySelector('.draw').style.backgroundColor = 'white';
+            document.querySelector('.clear').style.backgroundColor = 'white';
+        }
+    }
+    else {
+        document.querySelector('.eraser').style.backgroundColor = 'white';
     console.log(erase);
+    }
 });
 
 for(let i = 0; i < 256; i++) {
@@ -72,12 +119,10 @@ for(let i = 0; i < 256; i++) {
         if(erase === true) {
             console.log("erase: true");
             draw = false;
-            document.querySelector('.eraser').style.backgroundColor = 'blue';
             pixel[i].style.backgroundColor = 'white';
         }
         
         else {
-            document.querySelector('.eraser').style.backgroundColor = 'white';
             console.log("erase: false");   
             }
         
